aa5218d8e13da556480e720ee0ed016f73aedff0
Michael Ionov <imichaelionov@gmail.com>
2020-01-07 15:59:20 +0200

    problem 5 solved

---
+++ 5.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+# this is the answer
+num = 232792560
+while (True):
+    flag = 1
+    for i in range(1, 21):
+        if (num % i):
+            flag = 0
+            print(num)
+            break
+    if (flag):
+        print(num)
+        break
+    else:
+        num += 10
